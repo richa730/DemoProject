11f0e522fa26e05750ca5ee2da7a4191bee78870
RichaS <richarajwar230@gmail.com>
2024-09-23 15:27:35 +0530

    Conditions commit

---
+++ Condition.java
@@ -0,0 +1,20 @@
+public class Condition {
+
+    public static void main(String [] args)
+    {
+        ifelsecond(2);
+    }
+    public static int ifelsecond(int a)
+    {
+        if (a==2)
+        {
+            System.out.println("Condition is correct");
+        }
+        else
+        {
+            System.out.println("Condition is incorrect");
+        }
+        return a;
+    }
+
+}
