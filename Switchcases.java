2274893bbcbb3bd43026b831f72019e0c95d8e2d
RichaS <richarajwar230@gmail.com>
2024-09-24 16:52:07 +0530

    switch cases is added

---
+++ Switchcases.java
@@ -0,0 +1,31 @@
+public class Switchcases {
+
+    public static void main (String [] args)
+    {
+cases(4);
+    }
+
+    public static void cases(int n)
+    {
+       switch(n)
+       {
+        case 1:
+            System.out.println("Today is Monday");
+            break;
+           case 2:
+               System.out.println("Today is Monday");
+               break;
+
+           default:
+               System.out.println("Incorrect number is given");;
+
+
+       }
+
+
+    }
+}
+
+
+
+
