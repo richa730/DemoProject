4040b55103cd59ff709259aa794296857a1d2def
RichaS <richarajwar230@gmail.com>
2024-09-27 15:35:12 +0530

    For loop commit

--- Forloop.java
+++ Forloop.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Forloop {
+    public static void main (String [] args)
+    {
+        forlooptest(6);
+    }
+    public static void forlooptest(int i)
+    {
+        for (i=1; i<=7; i++)
+        {
+            System.out.println("Day" +i);
+            for (int j=9; j<=15; j++) {
+                System.out.println(" " +j + "-" + (j+1));
+            }
+        }//How a for loop works: initialize (set an initial value), check for condition, increment or decrement
+
+    }
 }
